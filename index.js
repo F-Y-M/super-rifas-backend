a96de18135b08fcd5027dc17e6b729f3f6c53478
fym <jan.juan@misena.edu.co>
2025-09-08 10:21:58 -0500

    cambios realizados a la conexion con google sheet

--- index.js
+++ index.js
@@ -41,9 +41,18 @@ async function getGoogleSheetsClient() {
       credentials: {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
         client_id: process.env.GOOGLE_CLIENT_ID,
-        client_secret: process.env.GOOGLE_CLIENT_SECRET,
         redirect_uris: [process.env.GOOGLE_REDIRECT_URI],
-        refresh_token: process.env.GOOGLE_REFRESH_TOKEN
+        refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+        
+        type: process.env.GOOGLE_ACCOUNT_TYPE,
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+        private_key: process.env.GOOGLE_CLIENT_SECRET,
+        auth_uri: process.env.GOOGLE_AUTH_URI,
+        token_uri: process.env.GOOGLE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER,
+        client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+        universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
       },
       scopes: SCOPES,
     });
