17320b5f8d18b152dba201ad5b2681bca3555d99
fym <jan.juan@misena.edu.co>
2025-09-08 10:21:58 -0500

    se corrigen variables de entorno y credenciales necesarios para conexion con google sheet

--- index.js
+++ index.js
@@ -39,11 +39,17 @@ async function getGoogleSheetsClient() {
   try {
     const auth = new google.auth.GoogleAuth({ 
       credentials: {
+        type: process.env.GOOGLE_TYPE,
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // ← Esto es CRÍTICO
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
         client_id: process.env.GOOGLE_CLIENT_ID,
-        client_secret: process.env.GOOGLE_CLIENT_SECRET,
-        redirect_uris: [process.env.GOOGLE_REDIRECT_URI],
-        refresh_token: process.env.GOOGLE_REFRESH_TOKEN
+        auth_uri: process.env.GOOGLE_AUTH_URI,
+        token_uri: process.env.GOOGLE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+        universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
       },
       scopes: SCOPES,
     });
